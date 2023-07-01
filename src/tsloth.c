301c218aa382cef8980d27c0715e9ef473d75399
jordipbou <jordipbou@protonmail.com>
2023-07-02 01:57:13 +0200

    Added a small test with typed stack. Performance does not degrade in an important way.

---
+++ src/tsloth.c
@@ -0,0 +1,118 @@
+#include<stdio.h>
+#include "tsloth.h"
+
+#ifdef _WIN32
+  #include <conio.h>
+#else
+	#include <unistd.h>
+	#include <termios.h>
+#endif
+
+/*
+ Source code for getch is taken from:
+ Crossline readline (https://github.com/jcwangxp/Crossline).
+ It's a fantastic readline cross-platform replacement, but only getch was
+ needed and there's no need to include everything else.
+*/
+#ifdef _WIN32
+int _getch (void) {	fflush (stdout); return _getch(); }
+#else
+int _getch ()
+{
+	char ch = 0;
+	struct termios old_term, cur_term;
+	fflush (stdout);
+	if (tcgetattr(STDIN_FILENO, &old_term) < 0)	{ perror("tcsetattr"); }
+	cur_term = old_term;
+	cur_term.c_lflag &= ~(ICANON | ECHO | ISIG); /* echoing off, canonical off, no signal chars */
+	cur_term.c_cc[VMIN] = 1;
+	cur_term.c_cc[VTIME] = 0;
+	if (tcsetattr(STDIN_FILENO, TCSANOW, &cur_term) < 0)	{ perror("tcsetattr"); }
+	if (read(STDIN_FILENO, &ch, 1) < 0)	{ /* perror("read()"); */ } /* signal will interrupt */
+	if (tcsetattr(STDIN_FILENO, TCSADRAIN, &old_term) < 0)	{ perror("tcsetattr"); }
+	return ch;
+}
+#endif
+
+void key(X* x) { S_lit(x, (C)_getch()); }
+void emit(X* x) { printf("%c", (B)S_drop(x)); }
+
+void hello(X* x) {
+	printf("Hello world!\n");
+}
+
+void memory(X* x) {
+	C n;
+	B* s;
+	switch (S_token(x)) {
+	case 'a': n = S_drop(x); S_lit(x, (C)malloc(n)); break;
+	case 'f': free((void*)S_drop(x)); break;
+	case 'd': s = (B*)S_drop(x); for (n = 0; n < 16; n++) { printf("%c", s[n]); }; break;
+	}
+}
+
+/* I do need a quotation to define a word and a quotation to find a word. */
+/*
+B* ROM =
+"[SLOTH]5[{.E1+]#_10E"
+"[Allocate a dictionary of 4096 bytes, and save on d variable]_"
+"4096Mad,"
+"[HERE is saved on h]_"
+"0h,"
+"[compile cell **,** is saved on c]_"
+"[d.h.+,h.~+h,]c,"
+"[and compile byte **c,** is saved on b]_"
+"[d.h.+.h.1+h,]b,";
+"[Latest word pointer will be saved on address 0 of the dictionary]_"
+"0c.$"
+"[Define TIB as next 80 bytes of dictionary]_"
+"80[0b.$]#"
+"[move string **cmove** is saved on s]_"
+"[[^^$:$;$1+$1+]#__]s,"
+"[Define COLON to be able to create words]_"
+"0,[Latest]_0;[Flags]_1;[Name length]_':;[Name]_h.d.,d,[Swap here and latest]_0;[Compile flags]_;[Compile name length]_s.$[Compile name]_";
+*/
+
+int main(int argc, char** argv) {
+	FILE* fptr;
+	B buf[255];
+	C i;
+	B* j;
+	X* x = S_init();
+	
+  EXT(x, 'K') = &key;
+  EXT(x, 'E') = &emit;
+  EXT(x, 'H') = &hello;
+	EXT(x, 'M') = &memory;
+
+	if (argc == 2 || argc == 3) {
+		fptr = fopen(argv[1], "r");
+		while (fgets(buf, 255, fptr)) {
+			S_eval(x, buf);
+			if (x->err != 0) {
+					printf("ERROR: %ld\n", x->err);
+					return;
+			}
+			x->err = 0;
+		}
+		printf("Ok "); for (i = 0; i < x->sp; i++) { printf("%ld ", x->s[i].v.c); } printf("\n");
+	}
+	/*
+	S_eval(x, ROM);
+	*/
+
+	if (argc == 1 || argc == 3) {
+		do {
+			fgets(buf, 255, stdin);
+			x->ip = buf;	
+			S_inner(x);
+			/*
+      memset(buf, 0, 255);
+      S_dump_X(buf, x, 50);
+      printf("%s\n", buf);
+			*/
+			if (x->err != 0) { printf("ERROR: %ld", x->err); return; }
+			printf("Ok "); for (i = 0; i < x->sp; i++) { printf("%ld ", x->s[i].v.c); } printf("\n");
+		} while(1);
+	}
+}
---
+++ src/tsloth.h
@@ -0,0 +1,222 @@
+#ifndef TSLOTH_VM
+#define TSLOTH_VM
+
+#include<stdint.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef char B;
+typedef intptr_t C;
+
+typedef struct O { C t; C l; union { C c; void* p; } v; } O;
+
+enum { INT = 2 } T;
+
+#define STACK_SIZE 64
+#define RSTACK_SIZE 64
+
+typedef struct _X { 
+  O* s; C sp; C ss;
+  B** r; C rp; C rs;
+	B* ip;
+  void (*ext[26])(struct _X*);
+  C err;
+  C tr;
+} X;
+
+#define EXT(x, l) (x->ext[l - 'A'])
+
+X* S_init() {
+	X* x = malloc(sizeof(X));
+  x->s = malloc(STACK_SIZE*sizeof(O));
+  x->r = malloc(RSTACK_SIZE*sizeof(C));
+	x->sp = x->rp = 0;
+  x->ss = STACK_SIZE;
+  x->rs = RSTACK_SIZE;
+  x->err = 0;
+  x->tr = 0;
+	return x;
+}
+
+C S_dump_S(B* s, X* x) {
+	C i = 0, t, n = 0;
+	while (i < x->sp) { s += t = sprintf(s, "%ld ", x->s[i++].v.c); n += t; }
+	return n;
+}
+
+C S_dump_CODE(B* s, B* a) {
+	C i = 0, t = 1;
+	if (a == 0) return 0;
+	while (t && a[i] && a[i] != 10) {
+		switch (a[i++]) {
+		case '[': t++; break;
+		case ']': t--; break;
+		}
+	}
+	return sprintf(s, "%.*s", (unsigned int)i, a);
+}
+
+C S_dump_R(B* s, X* x) {
+	C i, t = 1, n = 0;
+	s += t = S_dump_CODE(s, x->ip); n += t;
+	for (i = x->rp - 1; i >= 0; i--) {
+		*s++ = ' '; *s++ = ':'; *s++ = ' '; n += 3;
+		s += t = S_dump_CODE(s, x->r[i]); n += t;
+	}
+	return n;
+}
+
+C S_dump_X(B* s, X* x, unsigned int w) {
+	C t, n = 0;
+	s += t = S_dump_S(s, x); n += t;
+	*s++ = ':'; *s++ = ' '; n += 2;
+  s += t = S_dump_R(s, x); n += t;
+	return n;
+}
+
+void S_inner(X* x);
+
+B S_peek(X* x) { return x->ip == 0 || *x->ip == 0 ? 0 : *x->ip; }
+B S_token(X* x) { B tk = S_peek(x); x->ip++; return tk; }
+C S_is_digit(B c) { return c >= '0' && c <= '9'; }
+
+#define TS(x) ((x->s[x->sp - 1]).v.c)
+#define NS(x) ((x->s[x->sp - 2]).v.c)
+#define NNS(x) ((x->s[x->sp - 3]).v.c)
+
+void S_lit(X* x, C v) { x->s[x->sp++].v.c = v; x->s[x->sp].t = INT; }
+void S_dup(X* x) { S_lit(x, TS(x)); }
+void S_over(X* x) { S_lit(x, NS(x)); }
+void S_rot(X* x) { C t = NNS(x); NNS(x) = NS(x); NS(x) = TS(x); TS(x) = t; TS(x); }
+void S_swap(X* x) { C t = TS(x); TS(x) = NS(x); NS(x) = t; }
+C S_drop(X* x) { return x->s[--x->sp].v.c; }
+B* S_dropB(X* x) { return x->s[--x->sp].v.p; }
+
+void S_add(X* x) { NS(x) = NS(x) + TS(x); --x->sp; }
+void S_sub(X* x) { NS(x) = NS(x) - TS(x); --x->sp; }
+void S_mul(X* x) { NS(x) = NS(x) * TS(x); --x->sp; }
+void S_div(X* x) { NS(x) = NS(x) / TS(x); --x->sp; }
+void S_mod(X* x) { NS(x) = NS(x) % TS(x); --x->sp; }
+
+void S_and(X* x) { NS(x) = NS(x) & TS(x); --x->sp; }
+void S_or(X* x) { NS(x) = NS(x) | TS(x); --x->sp; }
+void S_not(X* x) { TS(x) = !TS(x); }
+
+void S_lt(X* x) { NS(x) = NS(x) < TS(x); --x->sp; }
+void S_eq(X* x) { NS(x) = NS(x) == TS(x); --x->sp; }
+void S_gt(X* x) { NS(x) = NS(x) > TS(x); --x->sp; }
+
+void S_push(X* x) { x->r[x->rp++] = x->ip; }
+void S_pop(X* x) { x->ip = x->r[--x->rp]; }
+void S_call(X* x) { B t = S_peek(x); if (t && t != ']') S_push(x); x->ip = (B*)S_drop(x); }
+void S_eval(X* x, B* q) { S_lit(x, (C)q); S_call(x); S_inner(x); }
+void S_if(X* x) { S_rot(x); if (!S_drop(x)) { S_swap(x); } S_drop(x); S_call(x); }
+void S_times(X* x) { B* q = (B*)S_drop(x); C n = S_drop(x); while (n-- > 0) { S_eval(x, q); } }
+
+void S_bstore(X* x) { B* a = (B*)S_drop(x); *a = (B)S_drop(x); }
+void S_cstore(X* x) { C* a = (C*)S_drop(x); *a = S_drop(x); }
+
+void S_bfetch(X* x) { S_lit(x, *((B*)S_drop(x))); }
+void S_cfetch(X* x) { S_lit(x, *((C*)S_drop(x))); }
+
+/* Just for testing purposes */
+void _bin_rec(X* x, B* c, B* t, B* r1, B* r2) {
+	S_eval(x, c);
+	if (S_drop(x)) {
+		S_eval(x, t);
+		return;
+	} else {
+		S_eval(x, r1);
+		_bin_rec(x, c, t, r1, r2);
+		S_swap(x);
+		_bin_rec(x, c, t, r1, r2);
+		S_eval(x, r2);
+		return;
+	}
+}
+
+void S_bin_rec(X* x) {
+	B* r2 = S_dropB(x);
+	B* r1 = S_dropB(x);
+	B* t = S_dropB(x);
+	B* c = S_dropB(x);
+	_bin_rec(x, c, t, r1, r2);
+}
+/* TESTING END */
+
+void S_parse_literal(X* x) { 
+	C n = 0; 
+	while (S_is_digit(S_peek(x))) { n = 10*n + (S_token(x) - '0'); } 
+	S_lit(x, n); 
+}
+
+void S_parse_quotation(X* x) { 
+	C t = 1; 
+	B c; 
+	S_lit(x, (C)(++x->ip)); 
+	while (t) { switch (S_token(x)) { case '[': t++; break; case ']': t--; break; } } 
+}
+
+void S_inner(X* x) {
+	B buf[255];
+	C frame = x->rp;
+	do {
+		/*
+		memset(buf, 0, 255);
+		S_dump_X(buf, x, 50);
+		printf("%s\n", buf);
+		*/
+		switch (S_peek(x)) {
+		case '0': case '1': case '2': case '3': case '4':
+		case '5': case '6': case '7': case '8': case '9': 
+      S_parse_literal(x); break;
+		case '[': 
+      S_parse_quotation(x); break;
+		case 0: case ']': 
+      if (x->rp > frame && x->rp > 0) S_pop(x);
+			else return;
+      break;
+    case 'A': case 'B': case 'C': case 'D': case 'E': case 'F':
+    case 'G': case 'H': case 'I': case 'J': case 'K': case 'L':
+    case 'M': case 'N': case 'O': case 'P': case 'Q': case 'R':
+    case 'S': case 'T': case 'U': case 'V': case 'W': case 'X':
+    case 'Y': case 'Z':
+      EXT(x, S_token(x))(x);
+      break;
+		default:
+			switch (S_token(x)) {
+      case '\'': S_lit(x, (C)S_token(x)); break;
+			case 's': S_swap(x); break;
+			case 'd': S_dup(x); break;
+			case 'o': S_over(x); break;
+			case 'r': S_rot(x); break;
+			case '_': S_drop(x); break;
+			case '+': S_add(x); break;
+			case '-': S_sub(x); break;
+			case '*': S_mul(x); break;
+			case '/': S_div(x); break;
+			case '%': S_mod(x); break;
+			case '&': S_and(x); break;
+			case '|': S_or(x); break;
+			case '!': S_not(x); break;
+			case '<': S_lt(x); break;
+			case '=': S_eq(x); break;
+			case '>': S_gt(x); break;
+			case '$': S_call(x); break;
+			case '?': S_if(x); break;
+			case '#': S_times(x); break;
+      case ':': S_bfetch(x); break;
+      case ';': S_bstore(x); break;
+			case '.': S_cfetch(x); break;
+			case ',': S_cstore(x); break;
+      case '~': S_lit(x, sizeof(C)); break;
+			case 'q': /* TODO: Just set error code */ exit(0); break;
+			/* Just for testing purposes */
+			case 'b': S_bin_rec(x); break;
+			/* TESTING END */
+			}
+		}
+	} while(1);
+}
+
+#endif
