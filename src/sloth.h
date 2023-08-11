1774ede593bfd1069dca0038a0c8adc2ff9fb76d
jordipbou <jordipbou@protonmail.com>
2023-08-12 01:36:11 +0200

    Removing anything that is not the basic VM

--- src/sloth.h
+++ src/sloth.h
@@ -3,7 +3,6 @@
 
 #include<stdint.h> /* intptr_t */
 #include<stdlib.h> /* malloc, free */
-#include<string.h> /* strncpy, memset, strncmp */
 
 #define V void /* inline void ? */
 
@@ -13,13 +12,10 @@ typedef intptr_t C;
 #define STACK_SIZE 64
 #define RSTACK_SIZE 64
 
-typedef struct _W { struct _W* l; B* c; B f; B s; B n[1]; } W;
-
 typedef struct _X {
   C* s; C sp; C ss;
   B** r; C rp; C rs;
 	B* ip;
-  B* b;
 	void (*key)(struct _X*);
 	void (*emit)(struct _X*);
   void (*trace)(struct _X*);
@@ -30,12 +26,6 @@ typedef struct _X {
 
 #define EXT(x, l) (x->ext[l - 'A'])
 
-#define S_DEFAULT_DICT_SIZE 8192
-
-#define S_BLOCK_SIZE(x) (((C*)x->b)[0])
-#define S_HERE(x) (((C*)x->b)[1])
-#define S_LATEST(x) (((C*)x->b)[2])
-
 X* S_init() {
 	X* x = malloc(sizeof(X));
   x->s = malloc(STACK_SIZE*sizeof(C));
@@ -45,7 +35,6 @@ X* S_init() {
   x->rs = RSTACK_SIZE;
   x->ext = malloc(26*sizeof(C));
   x->ip = 0;
-  x->b = 0;
   x->err = 0;
   x->tr = 0;
 	return x;
@@ -62,7 +51,6 @@ C S_is_digit(B c) { return c >= '0' && c <= '9'; }
 #define NNS(x) (x->s[x->sp - 3])
 
 V S_lit(X* x, C v) { x->s[x->sp++] = v; }
-#define S_STR(x, s) S_lit(x, (C)s); S_lit(x, (C)strlen(s))
 
 /* Parsing */
 
@@ -81,12 +69,6 @@ V S_parse_quotation(X* x) {
     case ']': t--; break;
     } 
   }
-}
-
-V S_parse_string(X* x) {
-  S_lit(x, (C)(x->ip));
-  while (S_token(x) != '"') {}
-  S_lit(x, (C)(x->ip - TS(x)) - 1);
 }
 
 /* Stack instructions */
@@ -117,8 +99,10 @@ V S_push(X* x) { x->r[x->rp++] = (B*)x->s[--x->sp]; }
 V S_pop(X* x) { x->s[x->sp++] = (C)x->r[--x->rp]; }
 V S_jump(X* x) { x->ip = (B*)S_drop(x); }
 V S_zjump(X* x) { S_swap(x); if (!S_drop(x)) S_jump(x); else S_drop(x); }
+
 #define TC(x, t) (x->ip && t && t != ']' && t != '}')
 V S_call(X* x) { B t = S_peek(x); if TC(x, t) x->r[x->rp++] = x->ip; S_jump(x); }
+
 B* S_return(X* x, C f) {
   if (x->rp > f && x->rp > 0) {
     x->ip = x->r[--x->rp];
@@ -137,23 +121,6 @@ V S_cfetch(X* x) { S_lit(x, *((C*)S_drop(x))); }
 
 V S_malloc(X* x) { S_lit(x, (C)malloc(S_drop(x))); }
 V S_free(X* x) { free((void*)S_drop(x)); }
-
-/* TODO: Modify inspect to use key/emit */
-V S_inspect(X* x) {
-  C i = 0, j;
-  B* a = (B*)S_drop(x);
-	for (i = 0; i < 6; i++) {
-		printf("\n%p ", a + 8*i);
-		for (j = 0; j < 8; j++) {
-			printf("%02X ", (unsigned char)a[8*i + j]);
-		}
-		for (j = 0; j < 8; j++) {
-			if (a[8*i + j] < 32 || a[8*i + j] > 126) printf(".");
-			else printf("%c", a[8*i + j]);
-		}
-	}
-	printf("\n");
-}
 
 V S_branch(X* x) { 
   S_rot(x); 
@@ -178,97 +145,6 @@ V S_while(X* x) {
   } while (1);
 }
 
-V S_create(X* x) {
-  C l = S_drop(x);
-  B* s = (B*)S_drop(x);
-	W* w = (W*)(x->b + S_HERE(x));
-	w->l = (W*)S_LATEST(x);
-	S_LATEST(x) = (C)w;
-	w->c = ((B*)w) + 2*sizeof(C) + 2 + l;
-	w->f = 0;
-	w->s = l;
-	strncpy(w->n, s, l);
-	S_lit(x, (C)w->c);
-	S_HERE(x) += 2*sizeof(C) + 2 + l;
-}
-
-V S_find(X* x) {
-  C l = S_drop(x);
-  B* s = (B*)S_drop(x);
-	W* w = (W*)S_LATEST(x);
-	while (w) {
-		if (w->s == l && !strncmp(w->n, s, l)) {
-			S_lit(x, (C)w);
-			return;
-		}
-		w = w->l;
-	}
-	S_lit(x, (C)s);
-	S_lit(x, l);
-	S_lit(x, 0);
-}
-
-V S_parse_symbol(X* x) {
-  C l = 0;
-  B* s = x->ip;
-  while (!isspace(S_token(x))) { l++; }
-  S_lit(x, (C)s);
-  S_lit(x, l);
-}
-
-V S_symbol(X* x, C c) {
-	W* w;
-	/* TODO: S_init_block(x); */
-  if (x->b == 0) {
-    x->b = malloc(S_DEFAULT_DICT_SIZE);
-    S_BLOCK_SIZE(x) = S_DEFAULT_DICT_SIZE;
-    S_HERE(x) = 3*sizeof(C);
-    S_LATEST(x) = 0;
-  }
-  S_parse_symbol(x);
-  S_find(x);
-  if (TS(x)) {
-    TS(x) = (C)(((W*)TS(x))->c);
-    if (c) S_call(x);
-  } else {
-    S_drop(x);
-    S_create(x);
-  }
-}
-
-V S_allot(X* x) { S_HERE(x) += S_drop(x); }
-
-V S_qcompile(X* x, C e) { 
-  C l = 0, t = 1; 
-  B* q = (B*)S_drop(x);
-  while (t) {
-    if (q[l] == '[') t++;
-    if (q[l] == ']') t--;
-    l++;
-  }
-  strncpy(x->b + S_HERE(x), q, l + e);
-  S_HERE(x) += l + e; 
-}
-
-void S_load_file(X* x) {
-  C l = S_drop(x); 
-  B* s = (B*)S_drop(x);
-  FILE* fptr;
-  B buf[1024];
-  B tmp[1024];
-  strncpy(tmp, s, l);
-  tmp[l] = 0;
-  printf("Trying to open: %s\n", tmp);
-  fptr = fopen(tmp, "r");
-  if (!fptr) {
-    printf("Can't load file\n");
-    return;
-  }
-  while (fgets(buf, 255, fptr)) {
-    S_eval(x, buf);
-  }
-}
-
 void S_inner(X* x) {
   C frame = x->rp;
   do {
@@ -290,10 +166,8 @@ void S_inner(X* x) {
     default:
       switch (S_token(x)) {
       case '[': S_parse_quotation(x); break;
-      case '"': S_parse_string(x); break;
       case '\'': S_lit(x, (C)S_token(x)); break;
       case '#': S_lit(x, *((C*)x->ip)); x->ip += sizeof(C); break;
-      /*case '#': S_count(x); break;*/
       case '@': S_lit(x, (C)(x->ip + ((B)S_token(x)))); break;
       /* Stacks */
       case '_': S_drop(x); break;
@@ -332,27 +206,15 @@ void S_inner(X* x) {
       case 'u': x->tr = 0; break;
       case 'v': x->tr = 1; break;
       /* Memory */
-      case 'b': S_lit(x, (C)(&x->b)); break;
-      case 'c': S_lit(x, sizeof(C)); break;
-      case '{': S_inspect(x); break;
       case 'f': S_free(x); break;
       case 'm': S_malloc(x); break;
       case '.': S_cfetch(x); break;
       case ':': S_bfetch(x); break;
       case ',': S_cstore(x); break;
       case ';': S_bstore(x); break;
-      /* Dictionary */
-      case 'a': S_allot(x); break;
-      case 'h': S_create(x); break;
-      case '\\': S_symbol(x, 0); break;
-      case '`': S_find(x); break;
-      case '$': S_symbol(x, 1); break;
-      case 'g': S_qcompile(x, -1); break;
-      case 'q': S_qcompile(x, 0); break;
       /* Input/output */
       case 'k': x->key(x); break;
       case 'e': x->emit(x); break;
-      case 'l': S_load_file(x); break;
       }
     }
   } while(1);
