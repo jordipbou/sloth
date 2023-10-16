3628a444f800b665bd437c65de12ff1668192026
jordipbou <jordipbou@protonmail.com>
2023-10-16 12:47:27 +0200

    Sync

--- src/sloth.c
+++ src/sloth.c
@@ -6,7 +6,7 @@ V hello(X* x) { printf("Hello world!\n"); }
 int main(int argc, char** argv) {
   int i;
   B buf[255];
-	X* x = init_VM(init_MEM());
+	X* x = init_SLOTH(init_VM(init_MEM()));
   /*EXT(x, 'H') = &hello;*/
 	while (1) {
     printf("> ");
--- src/vm.h
+++ src/vm.h
@@ -77,9 +77,32 @@ V inner(X* x);
 #define PUTL(x, a, l) (*((int64_t*)&x->m->h[a]) = (int64_t)l)
 #define PUTC(x, a, i) (*((C*)&x->m->h[a]) = i)
 
+V literal(X* x) {
+	L1(x, C, n);
+	if (n == 0) PUTB(x, x->m->hp++, '0');
+	else if (n == 1) PUTB(x, x->m->hp++, '1');
+  else if (n > INT8_MIN && n < INT8_MAX) {
+    PUTB(x, x->m->hp++, '#');
+    PUTB(x, x->m->hp++, n);
+  } else if (n > INT16_MIN && n < INT16_MAX) {
+    PUTB(x, x->m->hp++, '2');
+    PUTS(x, x->m->hp, n);
+		x->m->hp += 2;
+  } else if (n > INT32_MIN && n < INT32_MAX) {
+    PUTB(x, x->m->hp++, '4');
+    PUTI(x, x->m->hp, n);
+ 		x->m->hp += 4;
+ } else {
+    PUTB(x, x->m->hp++, '8');
+    PUTL(x, x->m->hp, n);
+ 		x->m->hp += 8;
+  }
+}
+
 #define TAIL(x) (x->ip >= MEM_SIZE || GETB(x, x->ip + 1) == ']' || GETB(x, x->ip + 1) == '}')
 V call(X* x) { L1(x, C, q); if (!TAIL(x)) x->r[x->rp++] = x->ip; x->ip = q; }
 V ret(X* x) { if (x->rp > 0) x->ip = x->r[--x->rp]; else x->ip = MEM_SIZE; }
+V zjump(X* x) { L1(x, C, b); if (!b) x->ip = b; }
 V eval(X* x, C q) { DPUSH(x, q); call(x); inner(x); }
 V quotation(X* x) { L1(x, C, n); DPUSH(x, x->ip); x->ip += n - 1; }
 
@@ -115,38 +138,91 @@ V bfetch(X* x) { L1(x, B*, a); DPUSH(x, *a); }
 V times(X* x) { L2(x, C, q, C, n); for(;n > 0; n--) eval(x, q); }
 V branch(X* x) { L3(x, C, f, C, t, C, b); b ? eval(x, t) : eval(x, f); }
 
-/*
 #define S_pr(s, n, f, a) { C t; s += t = sprintf(s, f, a); n += t; }
               
-V dump_code(X* x, I c) {
-  I t = 1;
-  while (t) {
-    if (GET(x, c) == '[') t++;
-    if (GET(x, c) == ']') t--;
-    printf("%c", GET(x, c));
-    c++;
+V dump_code(X* x, C c) {
+  C t = 1;
+	B token;
+  while (t && c < MEM_SIZE) {
+		token = GETB(x, c);
+    if (token == '[') t++;
+    if (token == ']') t--;
+		if (token == '#') {
+			printf("#%d", GETB(x, ++c));
+			c += 1;
+		} else if (token == '2') {
+		  printf("#%d", GETS(x, ++c));
+			c += 2;
+		} else if (token == '4') {
+		  printf("#%d", GETI(x, ++c));
+			c += 4;
+		} else if (token == '8') {
+		  printf("#%ld", GETL(x, ++c));
+			c += 8;
+		} else {
+	    printf("%c", token);
+	    c++;
+		}
   }
 }
 
 V dump_context(X* x) {
-  I i;
+  C i;
   B* t;
-  for (i = 0; i < x->dp; i++) printf("%ld ", x->ds[i]);
+  for (i = 0; i < x->dp; i++) printf("%ld ", x->d[i]);
   printf(": ");
   dump_code(x, x->ip);
   for (i = x->rp - 1; i >= 0; i--) {
     printf(" : ");
-    dump_code(x, x->rs[i]); 
+    dump_code(x, x->r[i]); 
   }
   printf("\n");
 }
-*/
 
 V parse_name(X* x) {
 	while (x->ipos < x->ilen && isspace(x->ibuf[x->ipos])) x->ipos++;
 	DPUSH(x, &x->ibuf[x->ipos]);
 	while (x->ipos < x->ilen && !isspace(x->ibuf[x->ipos])) x->ipos++;
 	DPUSH(x, &x->ibuf[x->ipos] - T(x));
+}
+
+V find_name(X* x) {
+	L2(x, C, l, B*, t);
+	S* s = x->m->l;
+	while (s) {
+		if (s->l == l && !strncmp(s->n, t, l)) break;
+		s = s->p;
+	}
+	DPUSH(x, t);
+	DPUSH(x, l);
+	DPUSH(x, s);
+}
+
+/* TODO: Word could not be found! */
+V compile(X* x) { 
+	printf("Executing compile\n");
+	parse_name(x); 
+	find_name(x); 
+	{ 
+		L3(x, S*, s, C, l, B*, t); 
+		printf("Word being compiled: %.*s\n", (int)l, t);
+		DPUSH(x, s->c); 
+		literal(x); 
+		PUTB(x, x->m->hp++, 'e'); 
+	} 
+}
+
+V tick(X* x) { parse_name(x); find_name(x); { L3(x, S*, s, C, _, B*, __); DPUSH(x, s->c); } }
+V see(X* x) { 
+	parse_name(x); 
+	find_name(x); 
+	{ 
+		L3(x, S*, s, C, l, B*, t);
+		printf(": %.*s ", (int)l, t);
+		dump_code(x, s->c); 
+		if ((s->f & IMMEDIATE) == IMMEDIATE) printf(" IMMEDIATE");
+		printf("\n");
+	} 
 }
 
 V colon(X* x) {
@@ -172,15 +248,33 @@ V semicolon(X* x) {
 	x->m->l->f &= ~HIDDEN;
 }
 
-V immediate(X* x) {
-	x->m->l->f |= IMMEDIATE;
+V immediate(X* x) {	x->m->l->f |= IMMEDIATE; }
+V recurse(X* x) { DPUSH(x, x->m->l->c); literal(x); PUTB(x, x->m->hp++, 'e'); }
+V ahead(X* x) { 
+	printf("AHEAD\n");
+	printf("Pushing here+1: %d\n", x->m->hp + 1);
+	DPUSH(x, x->m->hp + 1); 
+	printf("Saving 1024 for using 2 bytes\n");
+	DPUSH(x, 1024); 
+	literal(x); 
+	printf("Saved value: %d\n", GETS(x, T(x)));
+	printf("Current here: %d\n", x->m->hp);
+}
+V resolve(X* x) { 
+	L1(x, C, a); 
+	printf("RESOLVE\n");
+	printf("Address to set jump: %ld\n", a);
+	C d = x->m->hp - a; 
+	printf("Distance: %ld\n", d);
+	PUTS(x, a, d); 
+	printf("Value set to: %d\n", GETS(x, a));
 }
 
 #define PEEK(x) (GETB(x, x->ip))
 #define TOKEN(x) (GETB(x, x->ip++))
                 
 V step(X* x) {
-		/*dump_context(x);*/
+		dump_context(x);
   	switch (PEEK(x)) {
   	  case 'A': case 'B': 
   	  case 'C':	case 'D': 
@@ -202,17 +296,18 @@ V step(X* x) {
 				case 'e': call(x); break;
 				case '[': quotation(x); break;
   	  	case ']': case '}': ret(x); break;
+				case 'z': zjump(x); break;
   		  case '0': DPUSH(x, 0); break;
   		  case '1': DPUSH(x, 1); break;
-				case '#': /* TODO */ break;
- 				case '2': /* TODO */ break;
- 				case '4': /* TODO */ break;
- 				case '8': /* TODO */ break;
+				case '#': DPUSH(x, GETB(x, x->ip)); x->ip += 1; break;
+ 				case '2': DPUSH(x, GETS(x, x->ip)); x->ip += 2; break;
+ 				case '4': DPUSH(x, GETI(x, x->ip)); x->ip += 4; break;
+ 				case '8': DPUSH(x, GETL(x, x->ip)); x->ip += 8; break;
 			  case '_': DDROP(x); break;
   		  case 's': swap(x); break;
   		  case 'o': over(x); break;
   		  case 'd': dup(x); break;
-  		  case 'r': rot(x); break;
+  		  case '@': rot(x); break;
   		  case '+': add(x); break;
   		  case '-': sub(x); break;
   		  case '*': mul(x); break;
@@ -237,42 +332,20 @@ V step(X* x) {
 				case ':': colon(x); break;
 				case ';': semicolon(x); break;
 				case 'i': immediate(x); break;
+				case '`': recurse(x); break;
+				case 'a': ahead(x); break;
+				case 'r': resolve(x); break;
+				case 'c': compile(x); break;
+				case 'w':
+					switch (TOKEN(x)) {
+						case 's': see(x); break;
+					}
+					break;
 			}
   	}
 }
               
 V inner(X* x) { C rp = x->rp; while(x->rp >= rp && x->ip < MEM_SIZE) { step(x); } }
-
-V literal(X* x) {
-	L1(x, C, n);
-	if (n == 0) PUTB(x, x->m->hp++, '0');
-	else if (n == 1) PUTB(x, x->m->hp++, '1');
-  else if (n > INT8_MIN && n < INT8_MAX) {
-    PUTB(x, x->m->hp++, '#');
-    PUTB(x, x->m->hp++, n);
-  } else if (n > INT16_MIN && n < INT16_MAX) {
-    PUTB(x, x->m->hp++, '2');
-    PUTS(x, x->m->hp++, n);
-  } else if (n > INT32_MIN && n < INT32_MAX) {
-    PUTB(x, x->m->hp++, '4');
-    PUTI(x, x->m->hp++, n);
-  } else {
-    PUTB(x, x->m->hp++, '8');
-    PUTL(x, x->m->hp++, n);
-  }
-}
-
-V find_name(X* x) {
-	L2(x, C, l, B*, t);
-	S* s = x->m->l;
-	while (s) {
-		if (s->l == l && !strncmp(s->n, t, l)) break;
-		s = s->p;
-	}
-	DPUSH(x, t);
-	DPUSH(x, l);
-	DPUSH(x, s);
-}
 
 V evaluate(X* x, B* s) {
 	x->ibuf = s;	
@@ -285,9 +358,13 @@ V evaluate(X* x, B* s) {
 		if (T(x)) {
 			L3(x, S*, s, C, l, B*, t);
 			if (!x->s || (s->f & IMMEDIATE) == IMMEDIATE) {
+				printf("INTERPRETING %.*s\n", (int)l, t);
 				eval(x, s->c);
 			} else {
-				/* TODO: Compile */
+				printf("COMPILING %.*s\n", (int)l, t);
+				DPUSH(x, s->c);
+				literal(x);
+				PUTB(x, x->m->hp++, 'e');
 			}
 		} else {
 			L3(x, S*, _, C, l, B*, t);
@@ -321,5 +398,48 @@ V evaluate(X* x, B* s) {
 X* init_VM(M* m) { X* x = malloc(sizeof(X)); x->m = m; }
 X* init_EXT(X* x) { x->x = malloc(26*sizeof(C)); return x; }
 M* init_MEM() { return malloc(sizeof(M)); }
+X* init_SLOTH(X* x) {
+	evaluate(x, "\\: : $: \\;");
+	evaluate(x, ": ; $; \\;i");
+
+	evaluate(x, ": immediate $i ;");
+
+	evaluate(x, ": drop $_ ;");
+	evaluate(x, ": dup $d ;");
+	evaluate(x, ": over $o ;");
+	evaluate(x, ": swap $s ;");
+	evaluate(x, ": rot $@ ;");
+
+	evaluate(x, ": + $+ ;");
+	evaluate(x, ": - $- ;");
+	evaluate(x, ": * $* ;");
+	evaluate(x, ": / $/ ;");
+	evaluate(x, ": mod $% ;");
+
+	evaluate(x, ": < $< ;");
+	evaluate(x, ": = $= ;");
+	evaluate(x, ": > $> ;");
+
+	evaluate(x, ": and $& ;");
+	evaluate(x, ": or $| ;");
+	evaluate(x, ": invert $~ ;");
+
+	/* a must be executed, z must be compiled ?! */
+	/*
+	evaluate(x, ": if $az ; immediate");
+	evaluate(x, ": then $r ; immediate");
+	*/
+	evaluate(x, ": >mark $a ;");
+	evaluate(x, ": >resolve $r ;");
+	evaluate(x, ": compile $c ;");
+	evaluate(x, ": 0branch $z ;");
+	/* compile must compile into if ... WHAT THE FUCK ?! */
+	evaluate(x, ": if >mark compile 0branch ; immediate");
+	evaluate(x, ": then >resolve ; immediate");
+
+	evaluate(x, ": see $ws ;");
+
+	return x;
+}
 
 #endif
