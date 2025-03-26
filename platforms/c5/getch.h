64a9a9d57dd57a8fccf17873412dc4c1ee987e54
jordipbou <jordipbou@protonmail.com>
2025-03-26 11:00:59 +0100

    Added conditional words to Forth.

---
+++ platforms/c5/getch.h
@@ -0,0 +1,15 @@
+#include <termios.h>
+#include <unistd.h>
+
+/* Helper for linux, must add ifs for windows and all that*/
+int getch() {
+	struct termios oldt, newt;
+	int ch;
+	tcgetattr(STDIN_FILENO, &oldt);
+	newt = oldt;
+	newt.c_lflag &= ~(ICANON|ECHO);
+	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+	ch = getchar();
+	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+	return ch;
+}
