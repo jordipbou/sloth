2ec9c6ae11bcae8c6b7755a66dc51a018802e9b7
jordipbou <jordipbou@protonmail.com>
2025-06-12 22:37:34 +0200

    Modified sloth_raylib library to be like sqlite library.

---
+++ platforms/c/libs/raylib/repl.c
@@ -0,0 +1,26 @@
+#define SLOTH_IMPLEMENTATION
+#include"fsloth.h"
+#include"sloth_raylib.h"
+
+int main(int argc, char** argv) {
+	X* x = sloth_new();
+
+	sloth_bootstrap(x);
+
+	/* It's important to include ans.4th before */
+	/* bootstrapping raylib to allow evaluating */
+	/* forth code in bootstrap_raylib function. */
+	sloth_include(x, FORTH_PATH "ans.4th");
+
+	bootstrap_raylib(x);
+
+	sloth_include(x, ROOT_PATH "raylib.4th");
+
+	if (argc == 1) {
+		sloth_repl(x);
+	} else {
+		sloth_include(x, argv[1]);
+	}
+
+	sloth_free(x);
+}
--- platforms/c/libs/raylib/sloth_raylib.h
+++ platforms/c/libs/raylib/sloth_raylib.h
@@ -471,26 +471,3 @@ void bootstrap_raylib(X* x) {
 
 	sloth_evaluate(x, "forth-wordlist set-current");
 }
-
-int main(int argc, char** argv) {
-	X* x = sloth_new();
-
-	sloth_bootstrap(x);
-
-	/* It's important to include ans.4th before */
-	/* bootstrapping raylib to allow evaluating */
-	/* forth code in bootstrap_raylib function. */
-	sloth_include(x, FORTH_PATH "ans.4th");
-
-	bootstrap_raylib(x);
-
-	sloth_include(x, ROOT_PATH "raylib.4th");
-
-	if (argc == 1) {
-		sloth_repl(x);
-	} else {
-		sloth_include(x, argv[1]);
-	}
-
-	sloth_free(x);
-}
