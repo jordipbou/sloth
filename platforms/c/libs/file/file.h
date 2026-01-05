e7258062ce26b461400356309243ff2f704b8f87
jordipbou <jordipbou@protonmail.com>
2026-01-06 00:41:04 +0100

    Moved code on file.h inside platforms/c to its own library in platforms/c/libs/file

--- platforms/c/libs/file/file.h
+++ platforms/c/libs/file/file.h
@@ -1,39 +1,50 @@
 #include "sloth.h"
 
-/* -- File access words -------------------------------- */
+#include<errno.h>
 
-void sloth_paren_(X* x);
+#if defined(WINDOWS)
+#include <io.h>
+#define F_OK 0
+#define access _access
+#else
+#include<unistd.h>
+#endif
+
+/* -- File access methods ------------------------------ */
+
 void sloth_bin_(X* x);
-void sloth_close_file_(X* x);
-void sloth_create_file_(X* x);
-void sloth_delete_file_(X* x);
-void sloth_file_position_(X* x);
-void sloth_file_size_(X* x);
-void sloth_include_file_(X* x);
-void sloth_included_(X* x);
-void sloth_open_file_(X* x);
 void sloth_r_slash_o_(X* x);
 void sloth_r_slash_w_(X* x);
-void sloth_read_file_(X* x);
-void sloth_read_line_(X* x);
-void sloth_reposition_file_(X* x);
-void sloth_resize_file_(X* x);
-void sloth_s_quote_(X* x);
-void sloth_source_id_(X* x);
 void sloth_w_slash_o_(X* x);
-void sloth_write_file_(X* x);
-void sloth_write_line_(X* x);
 
-/* -- File access extension words ---------------------- */
+/* -- File operations ---------------------------------- */
 
-void sloth_file_status_(X* x);
+void sloth_create_file_(X* x);
+void sloth_open_file_(X* x);
+void sloth_close_file_(X* x);
+void sloth_file_position_(X* x);
+void sloth_file_size_(X* x);
+void sloth_reposition_file_(X* x);
 void sloth_flush_file_(X* x);
-void sloth_include_(X* x);
-void sloth_refill_(X* x);
+void sloth_resize_file_(X* x);
+void sloth_delete_file_(X* x);
 void sloth_rename_file_(X* x);
-void sloth_require_(X* x);
+void sloth_file_status_(X* x);
+
+/* -- Read operations ---------------------------------- */
+
+void sloth_read_line_(X* x);
+void sloth_read_file_(X* x);
+
+/* -- Write operations --------------------------------- */
+
+void sloth_write_line_(X* x);
+void sloth_write_file_(X* x);
+
+/* -- Loading scripts ---------------------------------- */
+
 void sloth_required_(X* x);
-void sloth_s_backslash_quote_(X* x);
+void sloth_require_(X* x);
 
 /* == Bootstrapping ==================================== */
 
