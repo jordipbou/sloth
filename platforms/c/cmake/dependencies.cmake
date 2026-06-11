4096228cfeaf650291713f6a844aba277bea905f
jordipbou <jordipbou@protonmail.com>
2026-06-11 12:37:32 +0200

    Removed the ALIAS, it was giving me problems.

--- platforms/c/cmake/dependencies.cmake
+++ platforms/c/cmake/dependencies.cmake
@@ -3,13 +3,13 @@ include(FetchContent)
 FetchContent_Declare(
 	cpnbi 
   GIT_REPOSITORY https://github.com/jordipbou/CPNBI.git
-	GIT_TAG    		 f8d9e468ea4999b472e6c7a682187f4e2ac34176
+	GIT_TAG d71eb9f32bfbc918b76ee56ba45ae77d482f347f 
 )
 
 FetchContent_Declare(
 	unity
-	GIT_REPOSITORY	https://github.com/ThrowTheSwitch/Unity.git
-	GIT_TAG					bbf8f3728a937c7627b8094de7ae13559d220ed5	
+	GIT_REPOSITORY https://github.com/ThrowTheSwitch/Unity.git
+	GIT_TAG bbf8f3728a937c7627b8094de7ae13559d220ed5	
 )
 
 # Fetches all declared deps in one call
